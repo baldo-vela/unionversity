fa080675430dcb3a6e61f272da0ff7b2d8b073ea
baldo-vela <74266847+baldo-vela@users.noreply.github.com>
2022-03-09 13:17:03 -0600

    Defines a custom Type

--- index.ts
+++ index.ts
@@ -1,3 +1,11 @@
 // Local File Dependancies
 import courses from "./courses";
 import studyGroups from "./studyGroups";
+
+type Course = {
+    id: number,
+    courseId: number,
+    title: string,
+    keywords: string[],
+    eventType: string
+};
