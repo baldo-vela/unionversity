6dec31aafea1c039014c57445abb25558dd13786
baldo-vela <74266847+baldo-vela@users.noreply.github.com>
2022-03-09 13:37:38 -0600

    Defines a new custom Type: StudyGroup

--- index.ts
+++ index.ts
@@ -1,10 +1,18 @@
 // Local File Dependancies
 import courses from "./courses";
 import studyGroups from "./studyGroups";
-
+// Note: Course & studyGroup differ by *one* key: studyGroupId and courseId which are pointers to each other
 type Course = {
     id: number,
     studyGroupId: number,
+    title: string,
+    keywords: string[],
+    eventType: string
+};
+
+type StudyGroup = {
+    id: number,
+    courseId: number,
     title: string,
     keywords: string[],
     eventType: string
